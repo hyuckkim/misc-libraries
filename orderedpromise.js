3f5c151df979e91195d92e7797dee29d949e4ee4
김광혁 <53367179+hyuckkim@users.noreply.github.com>
2025-01-18 15:04:07 +0900

    feat: set을 여러 번 호출하면 resolve를 여러 번 호출해 전부 반영할 수 있도록 수정

--- orderedpromise.js
+++ orderedpromise.js
@@ -1,24 +1,21 @@
 class OrderedPromise {
-    current = undefined;
+    tasks = [];
 
     set(fun) {
         return new Promise((resolve, reject) => {
-            this.current = (...args) => {
+            this.tasks.push((...args) => {
                 try {
                     resolve(fun(...args));
                 }
                 catch (e) {
                     reject(e);
                 }
-            };
+            });
         });
     }
     
     resolve(...args) {
-        if (!this.current) return;
-        const result = this.current(...args);
-
-        this.current = undefined;
-        return result;
+        if (this.tasks.length === 0) return;
+        return this.tasks.shift()(...args);
     }
 }
